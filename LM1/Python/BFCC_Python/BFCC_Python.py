076d67b7087a1270097430fef5ef45ef935c7b44
mzg73 <mzg73@psu.edu>
2023-03-19 15:28:56 -0400

    BFCC_Python.py completed

--- LM1/Python/BFCC_Python/BFCC_Python.py
+++ LM1/Python/BFCC_Python/BFCC_Python.py
@@ -1,2 +1,29 @@
-print("test")
-print("Hi")
+import string
+
+ciphertext = "Mjqqt NXY 402"  # This is a ciphertext version of the plaintext "Hello IST 402"
+key = 5  # The key to be guessed is 5
+
+alphabet_lower = string.ascii_lowercase  # lowercase alphabet
+alphabet_upper = string.ascii_uppercase  # uppercase alphabet
+
+
+def decrypt(ciphertext, key):
+    plaintext = ""
+    for char in ciphertext:
+        if char in alphabet_lower:
+            plaintext += alphabet_lower[(alphabet_lower.index(char) - key) % 26]
+        elif char in alphabet_upper:
+            plaintext += alphabet_upper[(alphabet_upper.index(char) - key) % 26]
+        else:
+            plaintext += char
+    return plaintext
+
+
+for i in range(0, 10):
+    if i == key:
+        plaintext = decrypt(ciphertext, key)
+        print(i, " was the key, brute force success")
+        print("Decrypted message:", plaintext)
+        break
+    else:
+        print(i, " is not the key")
