e4b4eb2d7f83f305a988764764b8d31e7041e1f8
mpc6332 <mpc6332@psu.edu>
2023-04-03 23:48:40 -0400

    File Changes

--- LM3/GoLang/TLS_Server/client.go
+++ LM3/GoLang/TLS_Server/client.go
@@ -9,9 +9,11 @@ func main() {
 	log.SetFlags(log.Lshortfile)
 
 	conf := &tls.Config{
+		// Bypasses verification of the server's certificate.
 		InsecureSkipVerify: true,
 	}
 
+	//  Dials the TCP server at "127.0.0.1:443" (Interestingly enough, 127.0.0.1 is a loopback address)
 	conn, err := tls.Dial("tcp", "127.0.0.1:443", conf)
 	if err != nil {
 		log.Println(err)
@@ -19,12 +21,14 @@ func main() {
 	}
 	defer conn.Close()
 
+	// Sends "hello" to the server
 	n, err := conn.Write([]byte("hello\n"))
 	if err != nil {
 		log.Println(n, err)
 		return
 	}
 
+	// Reads the sent message of "world" in a format of bytes
 	buf := make([]byte, 100)
 	n, err = conn.Read(buf)
 	if err != nil {
@@ -32,5 +36,6 @@ func main() {
 		return
 	}
 
+	// Ends up printing out "world"
 	println(string(buf[:n]))
 }
--- LM3/GoLang/TLS_Server/main.go
+++ LM3/GoLang/TLS_Server/main.go
@@ -11,13 +11,16 @@ func main() {
 	log.SetFlags(log.Lshortfile)
 
 	cer, err := tls.LoadX509KeyPair("server.crt", "server.key")
+	// If the certificate is not nil (GoLang's version of null), print out a log error
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
+	// Listens for TLS connections on port 443 (protocol is tcp)
 	config := &tls.Config{Certificates: []tls.Certificate{cer}}
 	ln, err := tls.Listen("tcp", ":443", config)
+	// If tcp listener is not nil (GoLang's version of null), print out a log error
 	if err != nil {
 		log.Println(err)
 		return
@@ -25,6 +28,7 @@ func main() {
 	defer ln.Close()
 
 	for {
+		// Starts actively listening for incoming connections (execute the "client.go" file to continue)
 		conn, err := ln.Accept()
 		if err != nil {
 			log.Println(err)
@@ -34,6 +38,7 @@ func main() {
 	}
 }
 
+// Method that starts on its own thread (can handle connections concurrently
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 	r := bufio.NewReader(conn)
