d7451727491e1c29689e55fb9d7f53f6f7ac9dd8
Marcus Gonzalez <marcusgonzalez267@gmail.com>
2023-04-04 21:55:18 -0400

    edited LM2

--- LM2/GoLang/GoLang ECB & OFB/GoLang ECB & OFB.go
+++ LM2/GoLang/GoLang ECB & OFB/GoLang ECB & OFB.go
@@ -7,15 +7,21 @@ import (
 
 /* an array with 4 rows and 2 columns*/
 var codebook = [4][2]int{{0b00, 0b01}, {0b01, 0b10}, {0b10, 0b11}, {0b11, 0b00}}
+var codebook2 = [4][2]int{{0b00, 0b10}, {0b01, 0b11}, {0b10, 0b00}, {0b11, 0b01}}
 var message = [4]int{0b00, 0b01, 0b10, 0b11}
+var message2 = []int{}
 var iv int = 0b10
 
-func codebookLookup(xor int) (lookupValue int) {
+func codebookLookup(codebookNum int, xor int) (lookupValue int) {
 	var i, j int = 0, 0
 	for i = 0; i < 4; i++ {
 		if codebook[i][j] == xor {
 			j++
-			lookupValue = codebook[i][j]
+			if codebookNum == 2 {
+				lookupValue = codebook2[i][j]
+			} else {
+				lookupValue = codebook[i][j]
+			}
 			break
 		}
 	}
@@ -26,14 +32,18 @@ func main() {
 	// ECB Mode
 	fmt.Printf("ECB: \n")
 	var x, i int = 0, 0
-	var xor int = 0
 	var lookupValue int = 0
 	for i = 0; i < 4; i++ {
-		xor = message[x]
-		lookupValue = codebookLookup(xor)
+		lookupValue = codebookLookup(2, message[x])
 		fmt.Printf("The ciphered value of %b is %b\n", message[x], lookupValue)
+		message2 = append(message2, lookupValue)
 		x++
-
+	}
+	x = 0
+	for i = 0; i < 4; i++ {
+		lookupValue = codebookLookup(2, message2[x])
+		fmt.Printf("The plaintext value of %b is %b\n", message2[x], lookupValue)
+		x++
 	}
 
 	// OFB Mode
@@ -42,7 +52,7 @@ func main() {
 	var keystream int = iv
 	var output int
 	for y = 0; y < 4; y++ {
-		keystream = codebookLookup(keystream)
+		keystream = codebookLookup(2, keystream)
 		output = keystream ^ message[y]
 		fmt.Printf("The ciphered value of %b is %b\n", message[y], output)
 	}
