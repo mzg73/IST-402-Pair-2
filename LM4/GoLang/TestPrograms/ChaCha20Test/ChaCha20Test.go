f76c6d0a34493acb2f898629962bf8803a647579
mpc6332 <mpc6332@psu.edu>
2023-04-11 23:52:55 -0400

    File Updates

--- LM4/GoLang/TestPrograms/ChaCha20Test/ChaCha20Test.go
+++ LM4/GoLang/TestPrograms/ChaCha20Test/ChaCha20Test.go
@@ -7,30 +7,30 @@ import (
 )
 
 func main() {
-	// Set the key and nonce
+	// Set the key (size of 32 bits) and nonce (size of 12 bits)
 	key := make([]byte, chacha20.KeySize)
-	copy(key[:], []byte("my-secret-key-123456789012345"))
+	copy(key[:], []byte("I-Like-Cryptography"))
 	nonce := make([]byte, chacha20.NonceSize)
-	copy(nonce[:], []byte("my-unique-nonce"))
+	copy(nonce[:], []byte("I-Love-PSU"))
 
-	// Create the ChaCha20 cipher object
-	chacha, err := chacha20.NewUnauthenticatedCipher(key, nonce)
+	// Create the ChaCha20 cipher object. Sends back an error if either the key or nonce sizes are incorrect/unsupported.
+	chacha, err := chacha20.NewUnauthenticatedCipher(key, nonce) // "Note that ChaCha20, like all stream ciphers, is not authenticated and allows attackers to silently tamper with the plaintext."
 	if err != nil {
 		panic(err)
 	}
 
 	// Set the plaintext message
-	plaintext := []byte("my secret message")
+	plaintext := []byte("Super secret message")
+	fmt.Println("\nPlain text:", string(plaintext))
 
 	// Encrypt the plaintext using the ChaCha20 cipher
 	ciphertext := make([]byte, len(plaintext))
 	chacha.XORKeyStream(ciphertext, plaintext)
 
-	// Print the encrypted text as a hex string
-	fmt.Println("Encrypted text:")
-	fmt.Println(hex.EncodeToString(ciphertext))
+	// Print the encrypted text as a hex string. Makes the encrypted output easier to read.
+	fmt.Println("Encrypted text:", hex.EncodeToString(ciphertext)) // "Note that ChaCha20, like all stream ciphers, is not authenticated and allows attackers to silently tamper with the plaintext."
 
-	// Decrypt the ciphertext using the same ChaCha20 cipher
+	// Decrypt the ciphertext by using (or recreating) the same ChaCha20 cipher. Sends back an error if either the key or nonce sizes are incorrect/unsupported.
 	decrypted := make([]byte, len(ciphertext))
 	chacha, err = chacha20.NewUnauthenticatedCipher(key, nonce)
 	if err != nil {
@@ -39,6 +39,5 @@ func main() {
 	chacha.XORKeyStream(decrypted, ciphertext)
 
 	// Print the decrypted text
-	fmt.Println("Decrypted text:")
-	fmt.Println(string(decrypted))
+	fmt.Println("Decrypted text:", string(decrypted))
 }
--- LM4/GoLang/TestUserInput/ChaCha20InputTest/ChaCha20Input.go
+++ LM4/GoLang/TestUserInput/ChaCha20InputTest/ChaCha20Input.go
@@ -4,43 +4,42 @@ import (
 	"bufio"
 	"encoding/hex"
 	"fmt"
+	"golang.org/x/crypto/chacha20"
 	"os"
 	"strings"
-
-	"golang.org/x/crypto/chacha20"
 )
 
 func main() {
-	// Sets the key and nonce
+	// Set the key (size of 32 bits) and nonce (size of 12 bits)
 	key := make([]byte, chacha20.KeySize)
-	copy(key[:], "my-secret-key-123456789012345")
+	copy(key[:], "I-Like-Cryptography")
 	nonce := make([]byte, chacha20.NonceSize)
-	copy(nonce[:], "my-unique-nonce")
+	copy(nonce[:], "I-Love-PSU")
 
-	// Create the ChaCha20 cipher object
+	// Create the ChaCha20 cipher object. Sends back an error if either the key or nonce sizes are incorrect/unsupported.
 	chacha, err := chacha20.NewUnauthenticatedCipher(key, nonce)
 	if err != nil {
 		panic(err)
 	}
 
 	// Prompts the user to input the encrypted message
-	fmt.Println("Enter the message to be encrypted:")
+	fmt.Println("\nEnter the message to be encrypted:")
 	reader := bufio.NewReader(os.Stdin)
 	plaintext, err := reader.ReadString('\n')
 	if err != nil {
 		panic(err)
 	}
 	plaintext = strings.TrimSuffix(plaintext, "\n")
+	fmt.Println("\nPlain text:", string(plaintext))
 
 	// Encrypt the message inputted from the user using the ChaCha20 cipher
 	ciphertext := make([]byte, len(plaintext))
 	chacha.XORKeyStream(ciphertext, []byte(plaintext))
 
-	// Print the encrypted text as a hex string
-	fmt.Println("Encrypted text:")
-	fmt.Println(hex.EncodeToString(ciphertext))
+	// Print the encrypted text as a hex string. Makes the encrypted output easier to read.
+	fmt.Println("Encrypted text:", hex.EncodeToString(ciphertext))
 
-	// Decrypt the ciphertext using the same ChaCha20 cipher
+	// Decrypt the ciphertext by using (or recreating) the same ChaCha20 cipher. Sends back an error if either the key or nonce sizes are incorrect/unsupported.
 	decrypted := make([]byte, len(ciphertext))
 	chacha, err = chacha20.NewUnauthenticatedCipher(key, nonce)
 	if err != nil {
@@ -49,6 +48,5 @@ func main() {
 	chacha.XORKeyStream(decrypted, ciphertext)
 
 	// Print the decrypted text to the console
-	fmt.Println("Decrypted text:")
-	fmt.Println(string(decrypted))
+	fmt.Println("Decrypted text:", string(decrypted))
 }
