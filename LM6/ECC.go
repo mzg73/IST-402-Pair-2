e8dfbd69ab49142c547d286bff5b1030cef170e3
mpc6332 <mpc6332@psu.edu>
2023-05-03 22:17:26 -0400

    Code rewrite using ECDH

--- LM6/ECC.go
+++ LM6/ECC.go
@@ -1,38 +1,56 @@
 package main
 
 import (
+	"bufio"
+	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
+	"encoding/hex"
 	"fmt"
-	"math/big"
+	"os"
+	"strings"
 )
 
 func main() {
-	curve := elliptic.P256()
-	priv, x, y, err := elliptic.GenerateKey(curve, rand.Reader)
-	if err != nil {
-		panic(err)
+
+	// Chooses the elliptical curve
+	curve := elliptic.P224()
+
+	// Generates the private keys for the sender and recipient
+	senderPrivateKey, _ := ecdsa.GenerateKey(curve, rand.Reader)
+	recipientPrivateKey, _ := ecdsa.GenerateKey(curve, rand.Reader)
+
+	// Obtains the recipient's public key
+	recipientPublicKey := recipientPrivateKey.PublicKey
+
+	// Performs ECDH key exchange to establish a shared secret. The shared secret is actually a new x-coordinate that was
+	// calculated by scalar multiplying the recipient's public key coordinates with the sender's private key's secret integer (the "D" value)
+	sharedX, _ := curve.ScalarMult(recipientPublicKey.X, recipientPublicKey.Y, senderPrivateKey.D.Bytes())
+
+	// Prompts the user to input the encrypted message
+	fmt.Println("\nEnter the message to be encrypted:")
+	reader := bufio.NewReader(os.Stdin)
+	plaintext, _ := reader.ReadString('\n')
+	plaintext = strings.TrimSuffix(plaintext, "\n")
+
+	// Applies XOR encryption to the plaintext message using the shared secret
+	fmt.Println("\nPlain text:", string(plaintext))
+	ciphertext := make([]byte, len(plaintext))
+	encryptionKey := sharedX.Bytes()
+	for i := range plaintext {
+		ciphertext[i] = plaintext[i] ^ encryptionKey[i%len(encryptionKey)]
 	}
-	fmt.Printf("Private key: %x\n", priv)
 
-	message := "IST 402"
-	fmt.Printf("Message: %s\n", message)
+	// Print the encrypted text as a hex string. Makes the encrypted output easier to read.
+	fmt.Println("Encrypted text:", hex.EncodeToString(ciphertext))
 
-	// encrypt
-	plaintext := []byte(message)
-	k := new(big.Int).SetBytes(priv)              // convert private key to big.Int
-	x1, _ := curve.ScalarBaseMult(k.Bytes())      // calculate public key
-	y1, _ := curve.ScalarMult(x, y, k.Bytes())    // calculate shared secret
-	ciphertext := elliptic.Marshal(curve, x1, y1) // encode public key
-	ciphertext = append(ciphertext, plaintext...) // append plaintext to encoded public key
-	fmt.Printf("Ciphertext: %x\n", ciphertext)
-
-	// decrypt
-	x1, y1 = elliptic.Unmarshal(curve, ciphertext[:curve.Params().BitSize/8+1])
-	sharedSecret, _ := curve.ScalarMult(x1, y1, priv)
-	decrypted := ciphertext[curve.Params().BitSize/8+1:]
-	for i := range decrypted {
-		decrypted[i] ^= sharedSecret.Bytes()[i%len(sharedSecret.Bytes())]
+	// Applies XOR decryption to the ciphertext using the shared secret
+	decryptionKey := sharedX.Bytes()
+	decrypted := make([]byte, len(ciphertext))
+	for i := range ciphertext {
+		decrypted[i] = ciphertext[i] ^ decryptionKey[i%len(decryptionKey)]
 	}
-	fmt.Printf("Decrypted: %s\n", string(decrypted))
+
+	// Print the decrypted text
+	fmt.Println("Decrypted text:", string(decrypted))
 }
